9f73c3984cf300fe54f8cc301bb9a8d7900d164c
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-01-28 03:39:10 -0200

    scripts for exploratory analysis of our CSV data

---
+++ src/strategies/strategies.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class plotly_template(str, Enum): 
+    PLOTLY= 'plotly'
+    PLOTLY_WHITE= 'plotly_white'
+    PLOTLY_DARK= 'plotly_dark'
+    GGPLOT2= 'ggplot2'
+    SEABORN= 'seaborn'
+
+
+
