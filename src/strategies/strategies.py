5c28c440606651f54f8b1abe9d75c9f02598988d
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-01-29 02:43:54 -0200

    The encoding strategies was added

--- src/strategies/strategies.py
+++ src/strategies/strategies.py
@@ -1,11 +1,15 @@
 from enum import Enum
 
-class plotly_template(str, Enum): 
+class plotly_templates_strategy(str, Enum): 
     PLOTLY= 'plotly'
     PLOTLY_WHITE= 'plotly_white'
     PLOTLY_DARK= 'plotly_dark'
     GGPLOT2= 'ggplot2'
     SEABORN= 'seaborn'
 
+class encoding_strategy(str, Enum): 
+    UTF8= 'utf-8'
+    ASCII= 'ascii'
+    LATIN1= 'latin-1'
 
 
