2da8594a0eb5193da9402440e12387a55bc2dab8
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-02-12 03:02:39 -0200

    The format and EDA as orchestrator work correctly

--- src/analysis/analysis.py
+++ src/analysis/analysis.py
@@ -41,7 +41,10 @@ class FormatDataAnalysis:
             pct_out= analysis_data[col].get('percent_outliers')
             ruta= ruta= analysis_data[col].get(f'analysis_path_{col}')
             
-            text_format+= f'- {col}: {n_out} outliers -> ({pct_out:.2f}%)\n- Sample: {ruta}\n'
+            if ruta is None: 
+                text_format+= f'- The number of outliers is {n_out}, so it is not necessary to make a boxplot for column {col}\n'
+            else: 
+                text_format+= f'- {col}: {n_out} outliers -> ({pct_out:.2f}%)\n- Sample: {ruta}\n'
         
         return text_format
     
@@ -52,7 +55,10 @@ class FormatDataAnalysis:
         r_val= analysis_data['correlation'].get('r_value')
         ruta= analysis_data['correlation'].get('analysis_path')
         
-        return f"- Strongest correlation: {col_a} vs {col_b} (r={r_val})\n- Complete matrix: {ruta}\n"
+        if ruta is None:
+            return f"- The correlation value is invalid. Correlation detected: {r_val}\n"
+        else:
+            return f"- Strongest correlation: {col_a} vs {col_b} (r={r_val})\n- Complete matrix: {ruta}\n" 
     
     @classmethod
     def category_insight_format(cls, analysis_data: Dict[str, Any]) -> str: 
@@ -97,26 +103,13 @@ Columns: {columns}
         return text
 
 class Analysis: 
-    def __init__(self, config: Dict[str, Any], overhead: float=1.8):
+    def __init__(self, frame: pl.DataFrame, config: Dict[str, Any], overhead: float=1.8):
         self.config= config
         
         self.file= self.config.data.input_path
         self.null_th= self.config.eda.thresholds.null_threshold
         
-        self.frame= self.get_frame(overhead=overhead)
-    
-    def get_frame(self, overhead: float=1.8) -> pl.DataFrame: 
-        memory_available= psutil.virtual_memory().available
-        estimated_memory= self.file.stat().st_size * overhead
-        
-        ratio= estimated_memory/memory_available
-        
-        if ratio > 0.65: 
-            logger.error(f'The file {self.file.name} is too large to be process. Ratio: {ratio:.2f}')
-            raise ValueError(f'The file {self.file.name} is too large to be process. Ratio: {ratio:.2f}')
-        else: 
-            logger.info(f'The frame for the file {self.file.name} was obtained succesfully')
-            return pl.read_csv(self.file)
+        self.frame= frame
     
     def eda_analysis(self) -> None:
         eda_class= EDA(frame=self.frame, null_threshold=self.null_th)
