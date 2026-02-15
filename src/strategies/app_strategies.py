416c36c5080f49550839563e8eb7b79243f1db54
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-02-15 02:24:33 -0200

    The strategies were changed based on the types of numerical and categorical data

--- src/strategies/app_strategies.py
+++ src/strategies/app_strategies.py
@@ -1,19 +1,23 @@
 from enum import Enum
 
-class operations_strategies(str, Enum): 
+class operations_strategies_num(str, Enum): 
     SUM= 'sum'
     AVG= 'avg'
     MAX= 'max'
     MIN= 'min'
     COUNT= 'count'
+
+class operations_strategies_cat(str, Enum): 
+    COUNT= 'count'
     UNIQUE= 'unique'
 
-class visualization_strategies(str, Enum): 
+class visualization_strategies_num(str, Enum): 
     HISTOGRAM= 'histogram'
     BOXPLOT= 'boxplot'
     HEATMAP= 'heatmap'
 
-
+class visualization_strategies_cat(str, Enum): 
+    HISTOGRAM= 'histogram'
 
 
 
