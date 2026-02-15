28eb7d66e450d7096d926c3f775238beb571f129
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-02-15 02:27:48 -0200

    main pipeline execution

---
+++ main.py
@@ -0,0 +1,32 @@
+from src.validation_config.read_config import ReadFile
+from src.analysis.analysis import Analysis
+from src.get_frame import get_frame
+#from src.streamlit.dynamic_functions import 
+from typing import List, Any
+
+config= ReadFile().read_file()
+file= config.data.input_path
+
+frame= get_frame(file=file)
+
+Analysis(frame=frame, config=config).run_analysis()
+
+def data() -> List[Any]: 
+    return [frame, file]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
